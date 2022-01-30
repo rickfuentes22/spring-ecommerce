37395b0944230d6445f0c9bf5d0328c8d84575b4
rickfuentes22 <anibal.fe47@gmail.com>
2022-01-30 15:24:21 -0300

    crear clases model, mapeo clases a tablas con jpa, diseño

--- src/main/java/com/creamlife/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/creamlife/ecommerce/SpringEcommerceApplication.java
@@ -1,12 +1,9 @@
 package com.creamlife.ecommerce;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
