a8660c89ca831d5369b47d0542a9b1aa654d7188
rickfuentes22 <anibal.fe47@gmail.com>
2022-01-30 16:02:21 -0300

    crear repository, clases de servicio, metodo show ProductoController

---
+++ src/main/java/com/creamlife/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,12 @@
+package com.creamlife.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.creamlife.ecommerce.model.Producto;
+
+@Repository
+
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+
+}
---
+++ src/main/java/com/creamlife/ecommerce/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.creamlife.ecommerce.service;
+
+import java.util.Optional;
+
+import com.creamlife.ecommerce.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto  producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+
+}
