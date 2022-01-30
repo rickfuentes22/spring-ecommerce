be1ee8b5853dc11b425ba6e2ce7c4ae7e42e1bfd
rickfuentes22 <anibal.fe47@gmail.com>
2022-01-30 18:52:24 -0300

    crea producto y editar producto

--- src/main/java/com/creamlife/ecommerce/service/ProductoService.java
+++ src/main/java/com/creamlife/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.creamlife.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.creamlife.ecommerce.model.Producto;
@@ -9,5 +10,6 @@ public interface ProductoService {
 	public Optional<Producto> get(Integer id);
 	public void update(Producto producto);
 	public void delete(Integer id);
+	public List<Producto> findAll();
 
 }
